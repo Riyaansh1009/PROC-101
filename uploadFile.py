a5040680a9adda2a582a228610d701efdc5c8f05
Riyaansh1009 <64411382+Riyaansh1009@users.noreply.github.com>
2021-09-14 23:01:50 +0530

    Add files via upload

--- uploadFile.py
+++ uploadFile.py
@@ -1,34 +1,38 @@
 import os
 import dropbox
 from dropbox.files import WriteMode
+
+
+def main():
+    accesstoken = ''
+    transferData = TransferData(access_token)
+
+    filefrom = str(input("Enter the folder path to transfer : "))
+    fileto = input("Enter full path of file to upload to dropbox: ")  
+
+    transferData.upload_file(file_from,file_to)
+    print("The file has been moved")
+
 class TransferData:
     def __init__(self,access_token):
-        self.access_token = access_token
-    
-    def upload(self,fileFrom,fileTo):
+        self.access_token =  access_token
+
+    def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
-        for root, dirs, files in os.walk(fileFrom):
-            path = input("Enter folder path")
-        f = open(fileFrom, 'rb')
-        dbx.files_upload(f.reas(),fileTo,mode=WriteMode('overwrite'))
-    
-def main():
-    access_token = input("Enter access token")
-    x = TransferData(access_token)
-
-    fileFrom = input("Enter path to file of whoch you want to upload")
-    fileTo = input("Enter path of cloud file you want to transfer the file to")
-    
-    x.upload(fileFrom,fileTo)
-    print("File has been moved to dropbox")
-
-
-            
-
-
 
         
+        for root, dirs, files in os.walk(filefrom):
 
-    
+            for filename in files:
+                
+                localpath = os.path.join(root, filename)
 
-    
+                relativepath = os.path.relpath(localpath, file_from)
+                dropboxpath = os.path.join(fileto, relative_path)
+               
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+
+
+
+main()
